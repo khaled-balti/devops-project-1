b28a55558e10fe9d14e6e8f38c265bc47e99b776
khaled-balti <khaledprogg20@gmail.com>
2025-08-19 14:19:03 +0100

    modifications

--- infra/hosted-zone/main.tf
+++ infra/hosted-zone/main.tf
@@ -2,10 +2,19 @@ variable "domain_name" {}
 variable "aws_lb_dns_name" {}
 variable "aws_lb_zone_id" {}
 
+# Create the hosted zone
+resource "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name          = var.domain_name
+  comment       = "Hosted zone for ${var.domain_name}"
+  force_destroy = true # allows deletion of records if zone is destroyed
+}
+//already created hosted zone
+/*
 data "aws_route53_zone" "dev_proj_1_jhooq_org" {
   name         = var.domain_name
   private_zone = false
 }
+*/
 
 resource "aws_route53_record" "lb_record" {
   zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "default" {
   storage_type            = "gp2"
   engine                  = "mysql"
   engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
+  instance_class          = "db.t3.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
@@ -29,4 +29,3 @@ resource "aws_db_instance" "default" {
   backup_retention_period = 0
   deletion_protection     = false
 }
-
